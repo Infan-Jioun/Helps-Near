0ec14d67f8bb2a627d91aa7d2e061c2539fc5b6a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-03-17 01:41:45 +0600

    create authController

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -2,11 +2,22 @@ import { Request, Response } from "express";
 import catchAsync from "../../../shared/catchAsync";
 import { authService } from "./auth.service";
 import { ICreateUserPayload } from "./auth.interface";
+import { sendResposne } from "../../../shared/sendResponse";
+import status from "http-status";
 
 const createUser = catchAsync(
     async (req: Request, res: Response) => {
         const payload = req.body;
-        const result = authService.createUser(payload as ICreateUserPayload)
+        const result = authService.createUser(payload as ICreateUserPayload);
+        sendResposne(res, {
+            httpStatusCode: status.CREATED,
+            success: true,
+            message: "Successfully user created",
+            data: result
+        })
     }
-    
+
 )
+export const authController = {
+    createUser
+}
