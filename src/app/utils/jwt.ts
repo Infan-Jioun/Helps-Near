a8856321ad0081b3b25dd45f9c2399adb23ff969
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-03-19 00:14:17 +0600

    create verifyToken

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -4,3 +4,20 @@ const createToken = (payload: JwtPayload, secret: string, { expiresIn }: SignOpt
     const token = jwt.sign(payload, secret, { expiresIn })
     return token
 }
+
+const verifyToken = (token: string, secret: string) => {
+    try {
+        const decoded = jwt.verify(token, secret) as JwtPayload;
+        return {
+            success: true,
+            data: decoded
+        }
+        
+    } catch (error: any) {
+        return {
+            success: false,
+            message: error.message,
+            error
+        }
+    }
+}
