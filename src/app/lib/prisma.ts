07386e147520aa6cbfc552b8c7c70418169d6e0a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-05-18 12:00:16 +0600

    chore: add newline for better code formatting in prisma.ts

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -3,6 +3,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { envConfig } from "../../config/env";
 import { PrismaClient } from "../../generated/prisma/client/client";
 
+
 const connectionString = envConfig.DATABASE_URL;
 
 const globalForPrisma = globalThis as unknown as {
